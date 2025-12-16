e32dccbbf1140014c0f810424418fc4f2609821e
Varghese <sojanvarghese2001@gmail.com>
2025-12-16 16:09:34 +0530

    Initial commit

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Alarm
-
-
-@admin.register(Alarm)
-class AlarmAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date', 'time', 'enabled', 'triggered')
-    list_filter = ('enabled', 'date')
-    search_fields = ('name',)
